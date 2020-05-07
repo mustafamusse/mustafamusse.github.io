21d29b56f5b012128eb0acce487ae03f636492e2
Mustafa Ali <62649206+mustafamusse@users.noreply.github.com>
2020-05-07 10:22:06 +0200

    Feedback

--- Feedback.php
+++ Feedback.php
@@ -1,7 +1,20 @@
 
 
 <?php
-  $name = $_POST
-  $email = $_POST
-  $message = $_POST
+  $email = $_POST ['email'];
+  $message = $_POST ['message'];
+
+  $email_from = 'Electra Game Website';
+  $email_subject = 'New Message From Game Website'
+  $email_body =
+                "Email: $email.\n".
+                "Message: $message.\n";
+
+  $to = "pyramidescapekth@gmail.com";
+  $headers = "From: $email_from\r\n";
+  $headers .= "Reply-to: $email\r\n"
+
+  mail($to,$email_subject,$email_body,$headers);
+
+  header("location: feedback.html");
 ?>
